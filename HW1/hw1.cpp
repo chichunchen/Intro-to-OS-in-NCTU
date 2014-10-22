8401274abefb1ec9d06e2902b362071e8a6c4bb7
chi chun chen <chi.chun_chen844@icloud.com>
2014-10-22 12:29:06 +0800

    round robin done, but the waiting time is wierd.

--- HW1/hw1.cpp
+++ HW1/hw1.cpp
@@ -2,18 +2,85 @@
 #include <fstream>
 using namespace std;
 
-string name_of_algo;
+const int MAX_SIZE = 10;
 
-class Process { 
+string name_of_algo;
+int process_count = 0;
+int waiting_time = 0;
+int turnaround_time = 0;
+
+class Process {
 public:
     int arrive_t;
     int burst_t;
+    int remain_t;
+    int begin;
+    int end;
 
     Process() {
         arrive_t = 0;
         burst_t = 0;
+        begin = 0;
+        end = 0;
+    }
+
+    void get_burst(int time)
+    {
+        burst_t = time;
+        remain_t = time;
     }
 } p[9]; 
+
+class ProcessQueue {
+private:
+    int item[MAX_SIZE];
+    int rear;
+    int front;
+public:
+    ProcessQueue();
+    void enqueue(int);
+    int dequeue();
+    int size();
+    void display();
+    bool isEmpty();
+    bool isFull();
+};
+ProcessQueue::ProcessQueue(){
+    rear = -1;
+    front = 0;
+}
+void ProcessQueue::enqueue(int data){
+    item[++rear] = data;
+}
+int ProcessQueue::dequeue(){
+    return item[front++];
+}
+void ProcessQueue::display(){
+    if(!this->isEmpty()){
+        for(int i=front; i<=rear; i++)
+            cout<<item[i]<<endl;
+    }else{
+        cout<<"Queue Underflow"<<endl;
+    }
+}
+int ProcessQueue::size(){
+    return (rear - front + 1);
+}
+bool ProcessQueue::isEmpty(){
+    if(front>rear){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+bool ProcessQueue::isFull(){
+    if(this->size()>=MAX_SIZE){
+        return true;
+    }else{
+        return false;
+    }
+}
 
 void readfile(string);
 void writefile();
@@ -31,7 +98,7 @@ int main()
     readfile(filename);
     // testformat();
     cpu_scheduler();
-    writefile();
+    // writefile();
 
     return 0;
 }
@@ -52,25 +119,26 @@ void readfile(string filename)
             getline(myfile, line);
             p[i].arrive_t = atoi(line.erase(0, 14).c_str());
             getline(myfile, line);
-            p[i].burst_t = atoi(line.erase(0, 12).c_str());
+            p[i].get_burst( atoi(line.erase(0, 12).c_str()) );
             
             i++;
         }
-
+        process_count = i - 1;
         myfile.close();
     }
-    else cout << "Unable to open file, check out the name of file."; 
+    else cout << "Unable to open file, check out the name of file.\n"; 
 }
 
 void writefile()
 {
     ofstream myfile;
     myfile.open ("0013428.txt");
-    myfile << "P1\n";
-    myfile << "P2\n";
-    myfile << "p3\n";
+    for(int i = 0; i < process_count; i++) {
+        myfile << "P" << i << "    " << p[i].begin << "-" << p[i].end << endl;
+    }
     myfile << "Average Waiting Time: \n";
     myfile << "Average Turnaround Time: \n";
+    myfile << "Gantt Chart: \n";
     myfile.close();
 }
 
@@ -95,17 +163,60 @@ void cpu_scheduler()
 
 void fcfs()
 {
-
+    
 }
 
 void srtf()
 {
-
+    
 }
 
 void rr(int quantum)
 {
-    cout << quantum << endl;
+    ofstream myfile;
+    myfile.open ("0013428.txt");
+
+    int time, i; 
+    int remain = process_count;
+    int flag = 0;
+    for(time = 0, i = 1; remain != 0; ) {
+
+        if(p[i].remain_t <= quantum && p[i].remain_t > 0)
+        {
+            myfile << "P" << i << "    " << time << "-";
+
+            time += p[i].remain_t;
+            p[i].remain_t = 0;
+            flag = 1;
+            // cout << "end of " << i << " : " << p[i].remain_t << endl;
+            
+            myfile << time << endl;
+        }
+        else if(p[i].remain_t > 0)
+        {
+            myfile << "P" << i << "    " << time << "-";
+
+            p[i].remain_t -= quantum;
+            time += quantum;
+            // cout << i << " : " << p[i].remain_t << endl;
+            myfile << time << endl;
+        }
+
+        if(p[i].remain_t == 0 && flag == 1)
+        {
+            remain--;
+            waiting_time += time - p[i].burst_t - p[i].arrive_t;
+            turnaround_time += time - p[i].arrive_t;
+            flag = 0;
+        }
+
+        if(i == process_count)
+            i = 1;
+        else if(p[i+1].arrive_t <= time)
+            i++;
+    }
+    myfile << "Average Waiting Time: " << waiting_time / process_count << endl;
+    myfile << "Average Turnaround Time: " << turnaround_time / process_count << endl;
 }
 
 void testformat()
